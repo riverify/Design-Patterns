b7eaf91e8440caad624855aa7a5221dd3110149a
Riverify <CryingRiver@foxmail.com>
2022-11-24 16:04:24 +0800

    解释器模式

--- src/com/river/interpreter/Test1.java
+++ src/com/river/interpreter/Test1.java
@@ -1,7 +1,7 @@
 package com.river.interpreter;
 
 /**
- * @Classname Test
+ * @Classname Test1
  * @Date 2022/11/24 15:23
  * @Created by river
  *
@@ -9,7 +9,7 @@ package com.river.interpreter;
  * 使用解释器模式实现一个简单的解释器，
  * 用于解释一段字符串，字符串中包含0和1，0代表假，1代表真，字符串中包含与运算和或运算(不考虑优先级)
  */
-public class Test {
+public class Test1 {
     public static void main(String[] args) {
         // 给出一个表达式
         String input = "1 and 0 or 1 and 1";
