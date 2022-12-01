43630f5c9bdc60de71ad4a4c5f9cab932012dd73
Riverify <CryingRiver@foxmail.com>
2022-12-01 15:48:55 +0800

    中介者模式

--- src/com/river/mediator/USA.java
+++ src/com/river/mediator/USA.java
@@ -9,6 +9,11 @@ package com.river.mediator;
  * 具体国家类 - 美国
  */
 public class USA extends Country {
+    /**
+     * 美国国家成员接收消息
+     *
+     * @param message 需要接收的消息
+     */
     @Override
     public void getDeclare(String message) {
         System.out.println("美国获得对方消息：" + message);
